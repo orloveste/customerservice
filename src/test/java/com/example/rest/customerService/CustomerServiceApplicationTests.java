72d54c9c2a9537162f45b00f85522b4e1ce2bbe9
orloveste <orlovevm@yahoo.com>
2021-01-21 12:23:03 +0200

    Initial commit

---
+++ src/test/java/com/example/rest/customerService/CustomerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rest.customerService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
